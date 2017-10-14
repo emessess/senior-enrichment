c109703a57d6bea63c43859463d49a34e23be7ba
Matthew Smith <mattsheridansmith@gmail.com>
2017-10-14 19:48:32 -0400

    api get routes completed for both students and campuses

--- server/api.js
+++ server/api.js
@@ -1,10 +1,28 @@
-'use strict'
-const api = require('express').Router()
-const db = require('../db')
+'use strict';
 
-// If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
-	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
-	// Ideally you would have something to handle this, so if you have time try that out!
-api.get('/hello', (req, res) => res.send({hello: 'world'}))
+const api = require('express').Router();
+const db = require('../db/models');
+const Student = db.Student;
+const Campus = db.Campus;
 
-module.exports = api
+api.get('/students', (req, res, next) => Student.findAll()
+  .then(students => res.json(students))
+  .catch(next)
+);
+
+api.get('/students/:id', (req, res, next) => Student.findById(req.params.id)
+  .then(student => res.json(student))
+  .catch(next)
+);
+
+api.get('/campuses', (req, res, next) => Campus.findAll()
+  .then(campuses => res.json(campuses))
+  .catch(next)
+);
+
+api.get('/campuses/:id', (req, res, next) => Campus.findById(req.params.id)
+  .then(campus => res.json(campus))
+  .catch(next)
+);
+
+module.exports = api;
